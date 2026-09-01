6ba6d91fe79d9f02cbae5c013461b1f66b8e6f5f
GZ-99 <gabrielvilchezvite@gmail.com>
2026-09-01 17:28:57 -0500

    chore: update Currency.cs

--- Acme.OOProgramming/Shared/Domain/Model/ValueObjects/Currency.cs
+++ Acme.OOProgramming/Shared/Domain/Model/ValueObjects/Currency.cs
@@ -2,5 +2,15 @@ namespace Acme.OOProgramming.Shared.Domain.Model.ValueObjects;
 
 public record Currency
 {
+    public string Code { get; set; }
+
+    public Currency(string code)
+    {
+        if (string.IsNullOrWhiteSpace(code))
+            throw new ArgumentNullException("Code cannot be null or empty", nameof(code));
+        
+        Code = code;
+        
+    }
     
 }
